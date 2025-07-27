3a96ce3215c213461caf4ff551e7d5f2dcabe672
Maximilian Waidelich <maximilian.waidelich@gmail.com>
2025-07-27 11:56:02 +0200

    better edge-to-edge window handling with Insets

--- app/src/main/java/com/maxwai/nclientv3/components/activities/CrashApplication.java
+++ app/src/main/java/com/maxwai/nclientv3/components/activities/CrashApplication.java
@@ -1,9 +1,18 @@
 package com.maxwai.nclientv3.components.activities;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowInsets;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.multidex.MultiDexApplication;
 
 import com.maxwai.nclientv3.BuildConfig;
@@ -40,6 +49,7 @@ public class CrashApplication extends MultiDexApplication {
         TagV2.initMinCount(this);
         TagV2.initSortByName(this);
         DownloadGalleryV2.loadDownloads(this);
+        registerActivityLifecycleCallbacks(new CustomActivityLifecycleCallback());
     }
 
     private void afterUpdateChecks(SharedPreferences preferences, String oldVersion) {
@@ -76,5 +86,53 @@ public class CrashApplication extends MultiDexApplication {
 
         ACRA.init(this, builder);
         ACRA.getErrorReporter().setEnabled(getSharedPreferences("Settings", 0).getBoolean(getString(R.string.key_send_report), false));
+    }
+
+    private static class CustomActivityLifecycleCallback implements ActivityLifecycleCallbacks {
+
+        @Override
+        public void onActivityCreated(@NonNull Activity activity, @Nullable Bundle savedInstanceState) {
+            View rootView = activity.getWindow().getDecorView().getRootView();
+            ViewCompat.setOnApplyWindowInsetsListener(rootView, (v, insets) -> {
+                Insets barsInsets = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                v.setPadding(
+                    barsInsets.left,
+                    barsInsets.top,
+                    barsInsets.right,
+                    barsInsets.bottom
+                );
+                return WindowInsetsCompat.CONSUMED;
+            });
+        }
+
+        @Override
+        public void onActivityDestroyed(@NonNull Activity activity) {
+
+        }
+
+        @Override
+        public void onActivityPaused(@NonNull Activity activity) {
+
+        }
+
+        @Override
+        public void onActivityResumed(@NonNull Activity activity) {
+
+        }
+
+        @Override
+        public void onActivitySaveInstanceState(@NonNull Activity activity, @NonNull Bundle outState) {
+
+        }
+
+        @Override
+        public void onActivityStarted(@NonNull Activity activity) {
+
+        }
+
+        @Override
+        public void onActivityStopped(@NonNull Activity activity) {
+
+        }
     }
 }
