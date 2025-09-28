b2eb4d6293f95ec7af48bbe6f46e1b18136826f8
Maximilian Waidelich <maximilian.waidelich@gmail.com>
2025-09-28 13:01:58 +0200

    removed varargs from function

--- app/src/main/java/com/maxwai/nclientv3/api/local/FakeInspector.java
+++ app/src/main/java/com/maxwai/nclientv3/api/local/FakeInspector.java
@@ -22,7 +22,7 @@ public class FakeInspector extends ThreadAsyncTask<LocalActivity, LocalActivity,
 
 
     @Override
-    protected LocalActivity doInBackground(LocalActivity... voids) {
+    protected LocalActivity doInBackground(LocalActivity[] voids) {
         if (!this.folder.exists()) return voids[0];
         publishProgress(voids[0]);
         File parent = this.folder;
@@ -35,7 +35,7 @@ public class FakeInspector extends ThreadAsyncTask<LocalActivity, LocalActivity,
     }
 
     @Override
-    protected void onProgressUpdate(LocalActivity... values) {
+    protected void onProgressUpdate(LocalActivity[] values) {
         values[0].getRefresher().setRefreshing(true);
     }
 
