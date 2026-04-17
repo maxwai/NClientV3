504c373bf03bc0fd471fdd285d9d65889f66117d
Maximilian Waidelich <maximilian.waidelich@gmail.com>
2026-04-17 09:42:12 +0200

    update app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,9 +35,9 @@ android {
         applicationId = "com.maxwai.nclientv3"
         // Format: MmPPbb
         // M: Major, m: minor, P: Patch, b: build
-        versionCode = 420400
+        versionCode = 420500
         multiDexEnabled = true
-        versionName = "4.2.4"
+        versionName = "4.2.5"
         vectorDrawables.useSupportLibrary = true
         proguardFiles("proguard-rules.pro")
     }
